bc5bc7bf46aa152a89bd9ef86e4b2de272b4b416
nageune <chkim@hsinfo.co.kr>
2025-03-31 14:43:13 +0900

    apply materui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Paper from '@mui/material/Paper';
+import { withStyles } from '@mui/material/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: '1600px'
+  }
+})
 
 const customers =[
   {
@@ -31,26 +49,28 @@ const customers =[
 
 class App extends Component {
   render() {
+    const {classes}=this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-          );
-        })
-      }
-    </div>
+      <Paper className={classes.root}>
+        <Table className={classes.Table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+          </TableBody>
+        </Table>
+        
+    </Paper>
   );
 }
 }
 
-export default App;
+export default withStyles(styles)(App);
